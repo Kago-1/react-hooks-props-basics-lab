cc505bb42acef9391233b8aee5a4a95664d543c2
Stephen <stephenkago10@gmail.com>
2022-09-21 13:16:04 +0300

    conditinal rendering

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
       <NavBar />
       <Home color={user.color} name={user.name} city={user.city} />
-      <About bio={user.bio} github={user.links.github} linkedin ={user.links.linkedin} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
